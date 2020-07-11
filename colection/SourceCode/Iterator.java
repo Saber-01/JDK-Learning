3ea8a5030040885abfafb401ab4a29c4785f03e4
Saber <821496291@qq.com>
2020-07-11 11:40:34 +0800

    Iterator源文件学习

---
+++ colection/SourceCode/Iterator.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright (c) 1997, 2013, Oracle and/or its affiliates. All rights reserved.
+ * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+package java.util;
+
+import java.util.function.Consumer;
+
+/**
+ * An iterator over a collection.  {@code Iterator} takes the place of
+ * {@link Enumeration} in the Java Collections Framework.  Iterators
+ * differ from enumerations in two ways:
+ *集合上的迭代器,代替了集合框架中的枚举，在两个方面不同于枚举。
+ *
+ * <ul>
+ *      <li> Iterators allow the caller to remove elements from the
+ *           underlying collection during the iteration with well-defined
+ *           semantics.
+ *           迭代器允许调用者在迭代期间使用定义良好的语义从基础集合中移除元素。
+ *      <li> Method names have been improved.
+ *      方法名已经改进
+ * </ul>
+ *
+ * <p>This interface is a member of the
+ * <a href="{@docRoot}/../technotes/guides/collections/index.html">
+ * Java Collections Framework</a>.
+ *此接口是java集合框架中的一员。
+ *
+ *
+ * @param <E> the type of elements returned by this iterator
+ *
+ * @author  Josh Bloch
+ * @see Collection
+ * @see ListIterator
+ * @see Iterable
+ * @since 1.2
+ */
+public interface Iterator<E> {
+    /**
+     * Returns {@code true} if the iteration has more elements.
+     * (In other words, returns {@code true} if {@link #next} would
+     * return an element rather than throwing an exception.)
+     *如果迭代器中还有更多的元素，则返回true。
+     *（换句话说，如果会返回true，则next方法会
+     * 返回元素，而不是引发异常。）
+     * @return {@code true} if the iteration has more elements
+     */
+    boolean hasNext();
+
+    /**
+     * Returns the next element in the iteration.
+     *  返回迭代器中的下一个元素。
+     * @return the next element in the iteration
+     * @throws NoSuchElementException if the iteration has no more elements
+     * 如果迭代器没有更多的元素，抛出NoSuchElementException
+     */
+    E next();
+
+    /**
+     * Removes from the underlying collection the last element returned
+     * by this iterator (optional operation).  This method can be called
+     * only once per call to {@link #next}.  The behavior of an iterator
+     * is unspecified if the underlying collection is modified while the
+     * iteration is in progress in any way other than by calling this
+     * method.
+     *从基础集合中移除此迭代器返回的最后一个元素（可选操作）。
+     * 每次调用{@link#next}时，只能调用此方法一次。
+     * 如果在迭代过程中以任何方式（而不是通过调用此方法）修改基础集合，
+     * 则迭代器的行为是不确定的。
+     * @implSpec
+     * The default implementation throws an instance of
+     * {@link UnsupportedOperationException} and performs no other action.
+     *
+     * @throws UnsupportedOperationException if the {@code remove}
+     *         operation is not supported by this iterator
+     *
+     * @throws IllegalStateException if the {@code next} method has not
+     *         yet been called, or the {@code remove} method has already
+     *         been called after the last call to the {@code next}
+     *         method
+     */
+    default void remove() {
+        throw new UnsupportedOperationException("remove");
+    }
+
+    /**
+     * Performs the given action for each remaining element until all elements
+     * have been processed or the action throws an exception.  Actions are
+     * performed in the order of iteration, if that order is specified.
+     * Exceptions thrown by the action are relayed to the caller.
+     *对剩余的每个元素执行给定的操作，直到所有元素都已处理或操作引发异常。
+     * 如果指定了迭代顺序，则按迭代顺序执行操作。
+     * 操作引发的异常会被转发到调用方。
+     * @implSpec
+     * <p>The default implementation behaves as if:
+     * <pre>{@code
+     *     while (hasNext())
+     *         action.accept(next());
+     * }</pre>
+     *
+     * @param action The action to be performed for each element
+     * @throws NullPointerException if the specified action is null
+     * @since 1.8
+     */
+    default void forEachRemaining(Consumer<? super E> action) {
+        Objects.requireNonNull(action);
+        while (hasNext())
+            action.accept(next());
+    }
+}
